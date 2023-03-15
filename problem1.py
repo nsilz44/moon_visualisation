88c25f6a08defa90a4249f05e21894690c8082da
Nsilz44 <n_stbss@hotmail.com>
2023-03-15 19:03:20 +0000

    added 3d perspective

--- problem1.py
+++ problem1.py
@@ -17,9 +17,9 @@ def contourLines(displacement,region,interval,annotation):
     fig.colorbar(frame=["x+lelevation", "y+lm"])
     fig.show()
 
-contourLines('ldem_4_uint.tif',[0,20,0,20],2000,12000)
+#contourLines('ldem_4_uint.tif',[0,20,0,20],2000,12000)
 img = 'lroc_color_poles_2k.tif'
-def baseimage(image,region):
+def baseImage(image,region):
     fig = pygmt.Figure()
     grid = image
     data = grid
@@ -33,4 +33,20 @@ def baseimage(image,region):
     fig.grdimage(grid=data,projection='M15c',cmap='gray',frame=True,region=region)   
     fig.show()
 
-baseimage(img,[0,20,0,20])
+#baseImage(img,[0,20,0,20])
+def threeDPerspective(displacement,region):
+    fig = pygmt.Figure()
+    grid = displacement
+    data = grid
+    xyz_df = pygmt.grd2xyz(grid=grid, output_type="pandas")
+    longAdd = xyz_df['x'].min() + xyz_df['x'].max()
+    xyz_df['x'] = xyz_df['x'].apply(lambda x: x*360/longAdd)
+    latAdd = xyz_df['y'].min() + xyz_df['y'].max()
+    xyz_df['y'] = xyz_df['y'].apply(lambda x: (x-(latAdd/2))*140/latAdd)
+    data = pygmt.xyz2grd(xyz_df,spacing=(720/longAdd,280/latAdd), region=[0, 360, -70, 70])
+    fig.grdview(grid=data,perspective=[130, 30], region=region,frame=["xa", "ya", "WSnE"],plane="1000+ggray",surftype="s",cmap="haxby",projection="M15c",zsize="1.5c")
+    fig.colorbar(perspective=True, frame=["x+lElevation", "y+lm"])
+    fig.show()
+
+threeDPerspective('ldem_4_uint.tif',[0,20,0,20])
+
