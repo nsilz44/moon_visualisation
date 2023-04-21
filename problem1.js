735d60b06f590301f8b5d2462587a1b7172710ff
Nsilz44 <n_stbss@hotmail.com>
2023-04-21 17:45:51 +0100

    added server js, html code for problem 1

---
+++ problem1.js
@@ -0,0 +1,60 @@
+
+url = 'http://localhost:8080/'
+var moon = d3.select('#basemoon');
+moon.on("click", function (e) {
+    var image = e.target;
+    rect = image.getBoundingClientRect();
+    offsetX = e.clientX - rect.left;
+    offsetY = e.clientY - rect.top;
+    minX = 78.31666564941406;
+    minY = 12;
+    maxX = 1281.316665649414;
+    maxY = 675;
+    coordX = (offsetX - minX) * (360/(maxX-minX));
+    coordY = ((offsetY - minY) * (140/(maxY-minY)) - 70) * -1;
+    console.log([coordX, coordY]);
+    document.getElementById('regionXL').value = coordX - 5;
+    document.getElementById('regionXR').value = coordX + 5;
+    document.getElementById('regionYB').value = coordY - 5
+    document.getElementById('regionYT').value = coordY + 5
+    displayImages();
+    createImages();
+})
+
+function displayImages(){
+    document.getElementById('contourimg').src = 'countourlines.png';
+    document.getElementById('threeD').src = 'threeDperspective.png';
+}
+
+function createImages(){
+    var regionXL = document.getElementById('regionXL').value;
+    var regionXR = document.getElementById('regionXR').value;
+    var regionYB = document.getElementById('regionYB').value;
+    var regionYT = document.getElementById('regionYT').value;
+    var colourmap = document.getElementById('colourmap').value;
+    var quality = document.getElementById('quality').value;
+    var contourinterval = document.getElementById('contourinterval').value;
+    var contourannotation = document.getElementById('contourannotation').value;
+    var viewAzimuth = document.getElementById('viewAzimuth').value;
+    var viewElevation = document.getElementById('viewElevation').value;
+    console.log([regionXL,regionXR,regionYB,regionYT],colourmap,quality,contourinterval,contourannotation,viewAzimuth,viewElevation)
+    async function getImages() {
+        let x = await fetch(url + new URLSearchParams({
+            regionXL: regionXL,
+            regionXR: regionXR,
+            regionYB: regionYB,
+            regionYT: regionYT,
+            cmap: colourmap,
+            quality:quality,
+            cInterval:contourinterval,
+            cAnnotation:contourannotation,
+            viewAzimuth:viewAzimuth,
+            viewElevation:viewElevation,
+            }));
+      }
+    getImages().then(
+        function(){
+        displayImages();
+        console.log('Done');
+        })
+}
--- problem1.py
+++ problem1.py
@@ -1,8 +1,11 @@
 import pygmt
 import pandas as pd
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import urllib.parse
 
 
-def contourLines(displacement,region,interval,annotation):
+def contourLines(displacement,region,interval,annotation,cmap,output_file):
     fig = pygmt.Figure()
     grid = displacement
     data = grid
@@ -12,14 +15,19 @@ def contourLines(displacement,region,interval,annotation):
     latAdd = xyz_df['y'].min() + xyz_df['y'].max()
     xyz_df['y'] = xyz_df['y'].apply(lambda x: (x-(latAdd/2))*140/latAdd)
     data = pygmt.xyz2grd(xyz_df,spacing=(720/longAdd,280/latAdd), region=[0, 360, -70, 70])
-    fig.grdimage(grid=data,cmap="haxby",frame=True,projection='M15c',region=region)
-    fig.grdcontour(grid=data,interval=interval,annotation=annotation)
-    fig.colorbar(frame=["x+lelevation", "y+lm"])
-    fig.show()
+    if cmap != 'none':
+        fig.grdimage(grid=data,cmap=cmap,frame=True,projection='M15c',region=region)
+    if annotation != 0:
+        fig.grdcontour(grid=data,interval=interval,projection='M15c',annotation=annotation,region=region)
+    else:
+        fig.grdcontour(grid=data,interval=interval,projection='M15c',region=region)
+    if cmap != 'none':
+        fig.colorbar(frame=["x+lelevation", "y+lm"])
+    fig.savefig(output_file)
 
-#contourLines('ldem_4_uint.tif',[0,20,0,20],2000,12000)
-img = 'lroc_color_poles_2k.tif'
-def baseImage(image,region):
+#contourLines('ldem_4_uint.tif',[0,20,0,20],2000,0,'haxby','countourLines.png')
+img = 'lroc_color_poles_8k.tif'
+def baseImage(image,region,output):
     fig = pygmt.Figure()
     grid = image
     data = grid
@@ -31,10 +39,10 @@ def baseImage(image,region):
     xyz_df['y'] = xyz_df['y'].apply(lambda x: (x-(latAdd/2))*140/latAdd)
     data = pygmt.xyz2grd(xyz_df,spacing=(720/longAdd,280/latAdd), region=[0, 360, -70, 70])
     fig.grdimage(grid=data,projection='M15c',cmap='gray',frame=True,region=region)   
-    fig.show()
+    fig.savefig(output)
 
-#baseImage(img,[0,20,0,20])
-def threeDPerspective(displacement,region):
+#baseImage(img,[0, 360, -70, 70],'BaseMoon.png')
+def threeDPerspective(displacement,region,viewAzimuth,viewElevation,cmap,output):
     fig = pygmt.Figure()
     grid = displacement
     data = grid
@@ -44,9 +52,46 @@ def threeDPerspective(displacement,region):
     latAdd = xyz_df['y'].min() + xyz_df['y'].max()
     xyz_df['y'] = xyz_df['y'].apply(lambda x: (x-(latAdd/2))*140/latAdd)
     data = pygmt.xyz2grd(xyz_df,spacing=(720/longAdd,280/latAdd), region=[0, 360, -70, 70])
-    fig.grdview(grid=data,perspective=[130, 30], region=region,frame=["xa", "ya", "WSnE"],plane="1000+ggray",surftype="s",cmap="haxby",projection="M15c",zsize="1.5c")
-    fig.colorbar(perspective=True, frame=["x+lElevation", "y+lm"])
-    fig.show()
+    if cmap != 'none':
+        fig.grdview(grid=data,perspective=[viewAzimuth,viewElevation], region=region,frame=["xa", "ya", "WSnE"],plane="1000+ggray",surftype="s",cmap=cmap,projection="M15c",zsize="1.5c")
+        fig.colorbar(perspective=True, frame=["x+lElevation", "y+lm"])
+    else:
+        fig.grdview(grid=data,perspective=[viewAzimuth,viewElevation], region=region,frame=["xa", "ya", "WSnE"],plane="1000+ggray",surftype="m",projection="M15c",zsize="1.5c")
+    fig.savefig(output)
 
-threeDPerspective('ldem_4_uint.tif',[0,20,0,20])
+#threeDPerspective('ldem_4_uint.tif',[0,10,0,10],135, 30,'geo','threeDperspective.png')
+hostName = "localhost"
+serverPort = 8080
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        params = self.path.split('&')
+        region = [float(params[0][10:]),float(params[1][9:]),float(params[2][9:]),float(params[3][9:])]
+        cmap = params[4][5:]
+        if params[5][8:] == 'low':
+            displacement = 'ldem_4_uint.tif'
+        else:
+            displacement = 'ldem_16_uint.tif'
+        interval = int(params[6][10:])
+        annotation = int(params[7][12:])
+        viewAzimuth = int(params[8][12:])
+        viewElevation = int(params[9][14:])
+        contourLines(displacement,region,interval,annotation,cmap,'countourLines.png')
+        threeDPerspective(displacement,region,viewAzimuth,viewElevation,cmap,'threeDperspective.png')
+        self.send_response(200)
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
+
+
+
+
 
